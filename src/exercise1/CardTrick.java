a45f7c14301be1a7ed9f5320ffc8cca39fff70c8
Krimal Patel <pakrimal@sheridancollege.ca>
2024-02-05 10:49:54 -0500

    third commit

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,8 @@
 package exercise1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -13,15 +16,46 @@ public class CardTrick {
     public static void main(String[] args) {
         
         Card[] hand = new Card[7];
+        Random random = new Random();
+        
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
+            
+            card.setValue(random.nextInt(13) + 1);
+            card.setSuit(Card.SUITS[random.nextInt(4)]);
+            hand[i] = card;
             //card.setValue(insert call to random number generator here)
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter value of the card to guess 1-10 for numbers, 11 for jack, 12 for queen, 13 for king:");
+        int yourResponse = scanner.nextInt();
+        System.out.println("Enter suit of the card to guess 1 for Hearts, 2 for Diamonds, 3 for Spades, 4 for Clubs:");
+        int response2 = scanner.nextInt();
+
+        for (Card card : hand) {
+            if (card.getValue() == yourResponse && card.getSuit().equals(Card.SUITS[response2 - 1])) {
+                printInfo();
+                break;
+            } else {
+                System.out.println("Sorry, you have guessed the wrong card");
+            }
+
+        }
+
+        scanner.close();
+
+            //card.setValue(insert call to random number generator here)
+            // 
+            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
+            //       Don't worry about duplicates at this point
+        
 
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
